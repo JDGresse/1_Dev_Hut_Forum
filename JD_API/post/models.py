f0c9db03ef86de73fea6e98f07405725a2156e1a
JDGresse <jdgresse01@gmail.com>
2023-11-09 14:51:32 +0200

    Edit model representation

--- JD_API/post/models.py
+++ JD_API/post/models.py
@@ -8,6 +8,12 @@ from django.contrib.auth.models import User
 class Category(models.Model):
     name = models.CharField(max_length=50)
 
+    class Meta:
+        verbose_name_plural = "categories"
+
+    def __str__(self):
+        return self.name
+
 
 class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -15,8 +21,14 @@ class Post(models.Model):
     body = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return self.title
+
 
 class Like(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     liked_by = models.ForeignKey(User, on_delete=models.CASCADE)
     liked_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.author} on '{self.post}'"
