f89c41c5536981fd65c6d4bcfad353419d0ddaa2
JDGresse <jdgresse01@gmail.com>
2023-11-10 18:04:09 +0200

    Refactoring based on postman test results.

--- JD_API/post/urls.py
+++ JD_API/post/urls.py
@@ -10,7 +10,7 @@ from post import views
 urlpatterns = [
     path("posts/", views.PostList.as_view()),
     path("posts/<int:pk>/", views.PostDetail.as_view()),
-    path("posts/likes", views.LikeList.as_view()),
+    path("posts/likes/", views.LikeList.as_view()),
     path("posts/<int:pk>/", views.PostDetail.as_view()),
     path("users/", views.UserList.as_view()),
     path("users/<int:pk>/", views.UserDetail.as_view()),
