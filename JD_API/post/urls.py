2b5eabc4f72992ae9c893bcff8f4c085e73531b5
JDGresse <jdgresse01@gmail.com>
2023-11-10 15:36:36 +0200

    Ready for postman testing.

--- JD_API/post/urls.py
+++ JD_API/post/urls.py
@@ -14,6 +14,7 @@ urlpatterns = [
     path("posts/<int:pk>/", views.PostDetail.as_view()),
     path("users/", views.UserList.as_view()),
     path("users/<int:pk>/", views.UserDetail.as_view()),
+    path("", views.api_root),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
